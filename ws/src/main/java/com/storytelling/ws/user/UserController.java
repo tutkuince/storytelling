6071b0000457ec32d83cc6c1fb4aa1a2d157e1a9
Tutku Ince <tutku.ince@outlook.com>
2023-09-16 23:25:46 +0300

    UserController and Application.properties has been updated

--- ws/src/main/java/com/storytelling/ws/user/UserController.java
+++ ws/src/main/java/com/storytelling/ws/user/UserController.java
@@ -1,13 +1,23 @@
 package com.storytelling.ws.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
+    private final UserService userService;
 
-    @PostMapping("/api/v1/users")
-    public void createUser() {
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
+    @PostMapping("/v1/users")
+    public void createUser(@RequestBody User user) {
+        userService.createUser(user);
     }
 }
