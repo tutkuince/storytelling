17522eb1b51dc2adac936409f7499a2f75f68394
Tutku Ince <tutku.ince@outlook.com>
2023-09-16 22:57:14 +0300

    we dont need @CrossOrigin after proxy configuration

--- ws/src/main/java/com/storytelling/ws/user/UserController.java
+++ ws/src/main/java/com/storytelling/ws/user/UserController.java
@@ -1,13 +1,11 @@
 package com.storytelling.ws.user;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    @CrossOrigin
     @PostMapping("/api/v1/users")
     public void createUser() {
 
