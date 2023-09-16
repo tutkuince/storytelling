4bba7b14be8071ed93cde8fd59c4332c433e6c7d
Tutku Ince <tutku.ince@outlook.com>
2023-09-16 23:22:54 +0300

    UserService has been created

---
+++ ws/src/main/java/com/storytelling/ws/user/UserService.java
@@ -0,0 +1,6 @@
+package com.storytelling.ws.user;
+
+public interface UserService {
+
+    void createUser(User user);
+}
---
+++ ws/src/main/java/com/storytelling/ws/user/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.storytelling.ws.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void createUser(User user) {
+        userRepository.save(user);
+    }
+}
