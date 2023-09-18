689aa9387298f09eaed0ee8e7f6390ad365338d5
Tutku Ince <tutku.ince@outlook.com>
2023-09-18 21:51:07 +0300

    new validation rules added to user service and controller

--- ws/src/main/java/com/storytelling/ws/user/UserController.java
+++ ws/src/main/java/com/storytelling/ws/user/UserController.java
@@ -4,6 +4,7 @@ import com.storytelling.ws.error.ApiError;
 import com.storytelling.ws.shared.GenericMessage;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -38,6 +39,18 @@ public class UserController {
         for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
             validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
         }
+        apiError.setValidationErrors(validationErrors);
+        return ResponseEntity.badRequest().body(apiError);
+    }
+
+    @ExceptionHandler(NotUniqueEmailException.class)
+    ResponseEntity<ApiError> handleNotUniqueEmailEx(NotUniqueEmailException exception) {
+        ApiError apiError = new ApiError();
+        apiError.setPath("/api/v1/users");
+        apiError.setMessage("Validation Error");
+        apiError.setStatus(400);
+        Map<String, String> validationErrors = new HashMap<>();
+        validationErrors.put("email", "E-mail in use");
         apiError.setValidationErrors(validationErrors);
         return ResponseEntity.badRequest().body(apiError);
     }
--- ws/src/main/java/com/storytelling/ws/user/UserServiceImpl.java
+++ ws/src/main/java/com/storytelling/ws/user/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.storytelling.ws.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,9 +17,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void createUser(User user) {
-        String encodedPassword = encodePassword(user.getPassword());
-        user.setPassword(encodedPassword);
-        userRepository.save(user);
+        try {
+            String encodedPassword = encodePassword(user.getPassword());
+            user.setPassword(encodedPassword);
+            userRepository.save(user);
+        } catch (DataIntegrityViolationException exception) {
+            throw new NotUniqueEmailException();
+        }
     }
 
     @Override
