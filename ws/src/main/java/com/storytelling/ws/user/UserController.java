09eeb290ac8cb71c9122d315b22b632794dcfbc7
Tutku Ince <tutku.ince@outlook.com>
2023-09-18 20:05:19 +0300

    E-mail validation control on backend

--- ws/src/main/java/com/storytelling/ws/user/UserController.java
+++ ws/src/main/java/com/storytelling/ws/user/UserController.java
@@ -24,16 +24,23 @@ public class UserController {
 
     @PostMapping("/v1/users")
     public ResponseEntity<?> createUser(@RequestBody User user) {
+        Map<String, String> validationErrors = new HashMap<>();
+        ApiError apiError = new ApiError();
+        apiError.setPath("/api/v1/users");
+        apiError.setMessage("Validation Error");
+        apiError.setStatus(400);
+
         if (user.getUsername() == null || user.getUsername().isEmpty()) {
-            ApiError apiError = new ApiError();
-            apiError.setPath("/api/v1/users");
-            apiError.setMessage("Validation Error");
-            apiError.setStatus(400);
-            Map<String, String> validationErrors = new HashMap<>();
             validationErrors.put("username", "Username cannot be null");
+        }
+        if (user.getEmail() == null || user.getEmail().isEmpty()) {
+            validationErrors.put("email", "email cannot be null");
+        }
+        if (!validationErrors.isEmpty()) {
             apiError.setValidationErrors(validationErrors);
             return ResponseEntity.badRequest().body(apiError);
         }
+
         userService.createUser(user);
         return ResponseEntity.ok(new GenericMessage("User is created"));
     }
