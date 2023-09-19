118475944428c5022ff7941ead6ede3970f0d31b
Tutku Ince <tutku.ince@outlook.com>
2023-09-19 16:50:48 +0300

    findByEmail method used in userservice

--- ws/src/main/java/com/storytelling/ws/user/UserServiceImpl.java
+++ ws/src/main/java/com/storytelling/ws/user/UserServiceImpl.java
@@ -31,4 +31,9 @@ public class UserServiceImpl implements UserService {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         return passwordEncoder.encode(password);
     }
+
+    @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
 }
