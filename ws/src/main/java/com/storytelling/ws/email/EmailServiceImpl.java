f8c9ce642acbbb55d12a939cff0429905d042b26
Tutku Ince <tutku.ince@outlook.com>
2023-09-30 22:21:16 +0300

    Created new mail template. The template used when mail sending

--- ws/src/main/java/com/storytelling/ws/email/EmailServiceImpl.java
+++ ws/src/main/java/com/storytelling/ws/email/EmailServiceImpl.java
@@ -2,9 +2,13 @@ package com.storytelling.ws.email;
 
 import com.storytelling.ws.configuration.StorytellingProperties;
 import jakarta.annotation.PostConstruct;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.util.Properties;
@@ -13,11 +17,26 @@ import java.util.Properties;
 public class EmailServiceImpl implements EmailService {
     private JavaMailSenderImpl mailSender;
     private StorytellingProperties properties;
+    private MessageSource messageSource;
 
     @Autowired
     public void setProperties(StorytellingProperties properties) {
         this.properties = properties;
     }
+
+    @Autowired
+    public void setMessageSource(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    String activationEmail = """
+            <html>
+                <body>
+                    <h1>${title}</h1>
+                    <a href="${url}">${clickHere}</a>
+                </body>
+            </html>
+            """;
 
     @PostConstruct
     public void initialize() {
@@ -34,11 +53,24 @@ public class EmailServiceImpl implements EmailService {
     @Override
     public void sendActivationEmail(String email, String activationToken) {
         String activationUrl = properties.getClient().host() + "/activation/" + activationToken;
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(properties.getEmail().from());
-        message.setTo(email);
-        message.setSubject("Account Activation");
-        message.setText(activationUrl);
-        this.mailSender.send(message);
+        var title = messageSource.getMessage("storytelling.mail.user.created.title", null, LocaleContextHolder.getLocale());
+        var clickHere = messageSource.getMessage("storytelling.mail.click.here", null, LocaleContextHolder.getLocale());
+
+        String mailBody = activationEmail
+                .replace("${url}", activationUrl)
+                .replace("${title}", title)
+                .replace("${clickHere}", clickHere);
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper message = new MimeMessageHelper(mimeMessage, "UTF-8");
+        try {
+            message.setFrom(properties.getEmail().from());
+            message.setTo(email);
+            message.setSubject(title);
+            message.setText(mailBody, true);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        this.mailSender.send(mimeMessage);
     }
 }
