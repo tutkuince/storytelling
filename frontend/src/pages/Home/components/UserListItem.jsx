bb0e09664f68b2b7a4b6504899cb79c83715ad91
Tutku Ince <tutku.ince@outlook.com>
2023-10-03 21:50:25 +0300

    Adding default profile image for users on fe part

--- frontend/src/pages/Home/components/UserListItem.jsx
+++ frontend/src/pages/Home/components/UserListItem.jsx
@@ -1,5 +1,14 @@
+import defaultProfileImage from "@/images/profile.png";
+
 export const UserListItem = ({ user }) => {
   return (
-    <li className="list-group-item list-group-item-action">{user.username}</li>
+    <li className="list-group-item list-group-item-action">
+      <img
+        src={defaultProfileImage}
+        width="30"
+        className="img-fluid rounded-circle shadow-sm"
+      />
+      <span className="ms-2">{user.username}</span>
+    </li>
   );
 };
