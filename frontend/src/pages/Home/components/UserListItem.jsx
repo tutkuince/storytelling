b4489bc338686a30c53b87006d53ce628b5c9755
Tutku Ince <tutku.ince@outlook.com>
2023-10-04 16:56:08 +0300

    URL created for each user.

--- frontend/src/pages/Home/components/UserListItem.jsx
+++ frontend/src/pages/Home/components/UserListItem.jsx
@@ -1,14 +1,19 @@
 import defaultProfileImage from "@/images/profile.png";
+import { Link } from "react-router-dom";
 
 export const UserListItem = ({ user }) => {
   return (
-    <li className="list-group-item list-group-item-action">
+    <Link
+      to={`/user/${user.id}`}
+      style={{ textDecoration: "none" }}
+      className="list-group-item list-group-item-action"
+    >
       <img
         src={defaultProfileImage}
         width="30"
         className="img-fluid rounded-circle shadow-sm"
       />
       <span className="ms-2">{user.username}</span>
-    </li>
+    </Link>
   );
 };
